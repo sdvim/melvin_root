0d270dfee3ffff84911c1ebdf6b6964cb8b9b8a7
Steve Della Valentina <s.dellavalentina@gmail.com>
2020-08-02 15:18:58 -0600

    Refactor store and router files

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import router from "./router.js";
+import store from "./store.js";
 
 Vue.config.productionTip = false;
 
