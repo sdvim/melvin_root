6d04513ba6822f6a56674439ddc723b60e80fc2b
Steve Della Valentina <s.dellavalentina@gmail.com>
2020-08-03 10:34:19 -0600

    Add type normalization to stats page

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -8,9 +8,7 @@
         :tabindex="index + 100"
       >
         <span class="stat__title">{{ stat.title }}</span>
-        <span class="stat__value">{{
-          Number(stat.value).toLocaleString()
-        }}</span>
+        <span class="stat__value">{{ normalize(stat) }}</span>
       </li>
     </ul>
   </div>
@@ -26,7 +24,48 @@ export default {
   // }
   computed: {
     stats() {
-      return this.$store.state.stats;
+      return this.$store ? this.$store.state.stats : [];
+    }
+  },
+  methods: {
+    normalize(stat) {
+      let result = "";
+      switch (stat.type) {
+        case "number":
+          result = Number(stat.value).toLocaleString();
+          break;
+        case "length":
+          result = Number(stat.value).toLocaleString() + " mi";
+          break;
+        case "time":
+          result = `${parseInt(stat.value / 86400)}d ${new Date(
+            stat.value * 1000
+          )
+            .toISOString()
+            .substr(11, 2)}h`;
+          break;
+        case "string":
+        default:
+          result = stat.value;
+          break;
+      }
+      if (stat.type == "time") {
+        let sec = parseInt(stat.value, 10);
+        const y = Math.floor(sec / (60 * 60 * 24 * 365));
+        sec %= 60 * 60 * 24 * 365;
+        const d = Math.floor(sec / (60 * 60 * 24));
+        sec %= 60 * 60 * 24;
+        const h = Math.floor(sec / (60 * 60));
+        sec %= 60 * 60;
+        const m = Math.floor(sec / 60);
+
+        result = "";
+        const units = { y, d, h, m };
+        for (const [key, value] of Object.entries(units)) {
+          if (value > 0) result += ` ${value}${key}`;
+        }
+      }
+      return result;
     }
   }
 };
@@ -42,6 +81,8 @@ ul {
   &:last-child {
     margin-bottom: 0;
   }
+  &:focus,
+  &:focus &__title,
   &:focus &__value {
     background: var(--highlight-color);
     color: var(--bg-color);
